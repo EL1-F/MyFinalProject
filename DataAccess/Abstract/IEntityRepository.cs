4b1f8315010c7a805e565ce7d353d3902ea9c5e8
elif coşkuner <elifipekcoskuner@gmail.com>
2023-10-07 15:30:36 +0300

    07/10/23

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T : class,IEntity,new()
+    {
+        //mesala kategorye göre getir ya da belli bir şarta göre getir demek için genel kullanım gibi
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        //Detaya gitmek için kullanılır
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
